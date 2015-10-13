a35eddfb004cb7467e781e7ac7d9507eff4aa51a
Rafael Ferreira <mail@rafaelferreira.pt>
2015-10-13 10:27:38 +0100

    Lesson3 ex3

--- Aulas Praticas/Rafael/licao3/MIPS_SystemC_v0.6.6/hazard.cpp
+++ Aulas Praticas/Rafael/licao3/MIPS_SystemC_v0.6.6/hazard.cpp
@@ -2,24 +2,22 @@
 #include "hazard.h"
 
 /**
- * Callback for the hazrd detection of \c hazard module.
+ * Callback for the hazard detection of \c hazard module.
  */
 void hazard::detect_hazard()
 {
 	//data hazards
 	if( rs.read()!=0 && rs.read()==WriteReg_exe.read() && RegWrite_exe.read()==true
 	    || rs.read()!=0 && rs.read()==WriteReg_mem.read() && RegWrite_mem.read()==true
-	    || rt.read()!=0 && rt.read()==WriteReg_exe.read() && RegWrite_exe.read()==true
-	    || rt.read()!=0 && rt.read()==WriteReg_mem.read() && RegWrite_mem.read()==true ) {
+	    || rt.read()!=0 && rt.read()==WriteReg_exe.read() && RegWrite_exe.read()==true && MemRead.read() ==false
+	    || rt.read()!=0 && rt.read()==WriteReg_mem.read() && RegWrite_mem.read()==true && MemRead.read() ==false) {
 
 		enable_pc.write(false);
 		enable_ifid.write(false);
 		reset_idexe.write(true);
-	}
-	else {
-	        enable_pc.write(true);
+	}else {
+	  enable_pc.write(true);
 		enable_ifid.write(true);
 		reset_idexe.write(false);
 	}
 }
-
